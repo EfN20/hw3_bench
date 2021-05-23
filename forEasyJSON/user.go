63a982709812e2d1595a0e45d63ea5602f5a0529
Azatkali <azatkali.16@gmail.com>
2021-05-23 17:38:30 +0600

    Assignment 2

---
+++ forEasyJSON/user.go
@@ -0,0 +1,8 @@
+package forEasyJSON
+
+//easyjson:json
+type userData struct {
+	Browsers []string `json:"browsers"`
+	Email string `json:"email"`
+	Name string `json:"name"`
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module azatkali/assignment2
+
+go 1.16
+
+require github.com/mailru/easyjson v0.7.7 // indirect
